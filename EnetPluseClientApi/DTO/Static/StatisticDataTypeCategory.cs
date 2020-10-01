18b00dd7d4ec187e6e9ae97e4949523c6eb2effa
james glasgow <glaschu1@users.noreply.github.com>
2020-10-01 21:15:37 +0000

    StatisticDataTypeCategory

---
+++ EnetPluseClientApi/DTO/Static/StatisticDataTypeCategory.cs
@@ -0,0 +1,26 @@
+using System;
+namespace EnetPluseClientApi.DTO.Static
+{
+    public enum StatisticDataTypeCategory
+    {
+        General = 1,
+        Performance,
+        Betting =5,
+        Oldest_Player,
+        Leading_Nation,
+        Leading_Nation_Goal,
+        Leading_Nation_Assists,
+        Leading_Nation_Yellow_Cards,
+        Leading_Nation_Red_Cards,
+        Leading_Own_Goal,
+        Talent_League,
+        Grass,
+        Clay,
+        Career,
+        Hard,
+        Current,
+        Carpet,
+        Season,
+        History,
+    }
+}
